8ce05fc691226c352654c4b3045c1534c1bee162
Daniel <daniel.rendox@gmail.com>
2024-05-12 22:37:55 +0300

    Add missing retrofit @Path annotation in GitHubApi

--- app/src/main/java/com/rendox/grocerygenius/network/GitHubApi.kt
+++ app/src/main/java/com/rendox/grocerygenius/network/GitHubApi.kt
@@ -5,6 +5,7 @@ import com.rendox.grocerygenius.network.model.NetworkChangeList
 import com.rendox.grocerygenius.network.model.ProductNetwork
 import okhttp3.ResponseBody
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Streaming
 
 interface GitHubApi {
@@ -20,7 +21,9 @@ interface GitHubApi {
     suspend fun getIconsArchive(): ResponseBody
 
     @GET("icons/{iconName}")
-    suspend fun getIconByName(iconName: String): ResponseBody
+    suspend fun getIconByName(
+        @Path("iconName") iconName: String
+    ): ResponseBody
 
     @GET("icons/icons_change_list.json")
     suspend fun getIconChangeList(): List<NetworkChangeList>
