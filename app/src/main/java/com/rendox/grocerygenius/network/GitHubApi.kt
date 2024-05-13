5037d098bfe7da926ee76239f9d373b4fed41c00
Daniel <daniel.rendox@gmail.com>
2024-05-13 14:23:29 +0300

    Apply localization to categories and products by adding language code to JSON file names

--- app/src/main/java/com/rendox/grocerygenius/network/GitHubApi.kt
+++ app/src/main/java/com/rendox/grocerygenius/network/GitHubApi.kt
@@ -5,12 +5,15 @@ import com.rendox.grocerygenius.network.model.NetworkChangeList
 import com.rendox.grocerygenius.network.model.ProductNetwork
 import okhttp3.ResponseBody
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Streaming
 
 interface GitHubApi {
 
-    @GET("category/categories.json")
-    suspend fun getCategories(): List<CategoryNetwork>
+    @GET("category/categories_{languageCode}.json")
+    suspend fun getCategories(
+        @Path("languageCode") languageCode: String = "en",
+    ): List<CategoryNetwork>
 
     @GET("category/categories_change_list.json")
     suspend fun getCategoriesChangeList(): List<NetworkChangeList>
@@ -25,8 +28,10 @@ interface GitHubApi {
     @GET("icons/icons_change_list.json")
     suspend fun getIconChangeList(): List<NetworkChangeList>
 
-    @GET("product/default_products.json")
-    suspend fun getProducts(): List<ProductNetwork>
+    @GET("product/default_products_{languageCode}.json")
+    suspend fun getProducts(
+        @Path("languageCode") languageCode: String = "en",
+    ): List<ProductNetwork>
 
     @GET("product/default_products_change_list.json")
     suspend fun getProductsChangeList(): List<NetworkChangeList>
