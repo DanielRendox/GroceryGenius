9f798c440295a084b18523e2c2b0548c780e6b2d
Danylo Biliaiev <danylo.biliaiev@jetbrains.com>
2025-11-19 01:55:29 +0100

    Build 0.1.4 (7)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.rendox.grocerygenius"
         minSdk = 21
         targetSdk = 35
-        versionCode = 6
-        versionName = "0.1.3"
+        versionCode = 7
+        versionName = "0.1.4"
 
         testInstrumentationRunner = "com.rendox.grocerygenius.testing.GroceryGeniusTestRunner"
         vectorDrawables {
